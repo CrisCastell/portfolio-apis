2c7b3eabafeb8c33bbc7efcdc44b3b265800fc7c
CrisCastell <cristiamcas2000@gmail.com>
2020-11-03 18:04:46 -0400

    Delete static dirs

--- apis/settings.py
+++ apis/settings.py
@@ -133,9 +133,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
