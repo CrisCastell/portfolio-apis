5df52740ec7a5e1d4931f4f7ece669dc2d422dbc
CrisCastell <cristiamcas2000@gmail.com>
2020-11-03 19:05:43 -0400

    Added django-heroku

--- apis/settings.py
+++ apis/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,3 +139,5 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+django_heroku.settings(locals())
